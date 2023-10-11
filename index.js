a61db85c8287a114d600c5691f512f9fe822e6a2
Caesar <caesar.keene@gmail.com>
2023-10-11 16:58:03 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Write your code here 
+ 
+let num1 = 2;
+let num2 = 31;
+const multiply = num1*num2; 
 
-// Write your code here
+const random = Math.floor(Math.random()) +1
+
+let num3 = 8;
+let num4 = 2; 
+const mod = num3/num4 
+console.log(mod) 
+
+const max = Math.max(1,3,7,6,12,20); 
+console.log(max) 
